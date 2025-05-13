5eea5b0daca3f5bff02951898be11102a701740b
Xin Xu <xin.xu97@outlook.com>
2025-05-13 17:52:19 +0200

    change

--- Continuous_Development/terraform/backend/main.tf
+++ Continuous_Development/terraform/backend/main.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "tf_state_bucket" {
-  bucket = var.backend_bucket
-  force_destroy = true
-  tags = {
-    Name        = "Terraform State Bucket"
-    Environment = "stg"
-  }
-}
+# resource "aws_s3_bucket" "tf_state_bucket" {
+#   bucket = var.backend_bucket
+#   force_destroy = true
+#   tags = {
+#     Name        = "Terraform State Bucket"
+#     Environment = "stg"
+#   }
+# }
